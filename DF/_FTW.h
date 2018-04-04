e9d5408501285f92e287c0b773c402d6771f5efc
SamipJ <samipjasani@gmail.com>
2018-04-04 15:17:22 +0530

    rearrange files

--- DF/_FTW.h
+++
@@ -1,9 +0,0 @@
-// #define _DICT_H 500
-#include <ftw.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include "_Dict.h"
-
-int fileproc(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf);
